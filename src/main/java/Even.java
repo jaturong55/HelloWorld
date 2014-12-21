3879ca873bccd544afa450f31e137cc68bb1f6a1
vViRuSs <vViRuSs@192.168.1.4>
2014-12-21 22:55:45 +0700

    first even commit

---
+++ src/main/java/Even.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author vViRuSs
+ */
+public class Even {
+    public static void main(String[] args) {
+        System.out.println("input number :");
+        Scanner scanner = new Scanner (System.in);
+        int number = scanner.nextInt();
+        
+        for(int i=0;i<=number;i++){
+            if(i%2 == 0){
+                System.out.println("result :" +i);
+            }
+        }
+    }
+    
+}
